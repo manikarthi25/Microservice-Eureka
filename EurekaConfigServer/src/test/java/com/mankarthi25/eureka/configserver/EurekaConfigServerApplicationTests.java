b07fb9cb058982314755edb1c2e3290c77d2f3bb
mani <manikarthi25@gmail.com>
2020-09-14 14:32:11 +0530

    Eureka Config Server

---
+++ EurekaConfigServer/src/test/java/com/mankarthi25/eureka/configserver/EurekaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mankarthi25.eureka.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
