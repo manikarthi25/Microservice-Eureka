d6e12abf0cdbba9398ff99c9368e836c4028995b
mani <manikarthi25@gmail.com>
2020-09-15 00:51:01 +0530

    last commit

--- PhotoAppApiAlbums/src/test/java/com/manikarthi25/eureka/albums/EurekaClientAlbumsApplicationTests.java
+++ PhotoAppApiAlbums/src/test/java/com/manikarthi25/eureka/albums/EurekaClientAlbumsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.appsdeveloperblog.photoapp.api.albums.PhotoAppApiAlbums;
+package com.manikarthi25.eureka.albums;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PhotoAppApiAlbumsApplicationTests {
+public class EurekaClientAlbumsApplicationTests {
 
 	@Test
 	public void contextLoads() {
